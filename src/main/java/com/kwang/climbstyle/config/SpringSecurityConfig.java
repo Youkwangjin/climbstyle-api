cba01d48e6adf8dcb156fa990af03bd82f6f710e
Youkwangjin <kwangjin5468@naver.com>
2025-11-26 18:06:22 +0900

    Feat: JSON 기반 로그인 요청 처리를 위한 CustomUserJsonLoginFilter 추가

--- src/main/java/com/kwang/climbstyle/config/SpringSecurityConfig.java
+++ src/main/java/com/kwang/climbstyle/config/SpringSecurityConfig.java
@@ -1,15 +1,37 @@
 package com.kwang.climbstyle.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kwang.climbstyle.security.filter.CustomUserJsonLoginFilter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final ObjectMapper objectMapper;
+
+    private final AuthenticationSuccessHandler loginSuccessHandler;
+
+    public SpringSecurityConfig(AuthenticationConfiguration authenticationConfiguration,
+                                ObjectMapper objectMapper,
+                                @Qualifier("CustomLoginSuccessHandler") AuthenticationSuccessHandler loginSuccessHandler) {
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.objectMapper = objectMapper;
+        this.loginSuccessHandler = loginSuccessHandler;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,6 +56,19 @@ public class SpringSecurityConfig {
                 .anyRequest().permitAll()
                 );
 
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .addFilterBefore(new CustomUserJsonLoginFilter(authenticationManager(authenticationConfiguration), objectMapper, loginSuccessHandler),
+                        UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
---
+++ src/main/java/com/kwang/climbstyle/security/filter/CustomUserJsonLoginFilter.java
@@ -0,0 +1,82 @@
+package com.kwang.climbstyle.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kwang.climbstyle.domain.auth.dto.request.LoginRequest;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class CustomUserJsonLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    private static final String HTTP_METHOD_POST = "POST";
+
+    private static final String LOGIN_URL = "/api/v1/login";
+
+    private static final String CONTENT_TYPE = "application/json";
+
+    private static final RequestMatcher DEFAULT_AUTH_MATCHER = PathPatternRequestMatcher.withDefaults()
+            .matcher(HttpMethod.POST, LOGIN_URL);
+
+    private final ObjectMapper objectMapper;
+
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    public CustomUserJsonLoginFilter(AuthenticationManager authenticationManager, ObjectMapper objectMapper, AuthenticationSuccessHandler authenticationSuccessHandler) {
+        super(DEFAULT_AUTH_MATCHER, authenticationManager);
+        this.objectMapper = objectMapper;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        if (request.getContentType() == null || !CONTENT_TYPE.equals(request.getContentType())) {
+            throw new AuthenticationServiceException("Unsupported content type: " + request.getContentType());
+        }
+
+        if (!request.getMethod().equals(HTTP_METHOD_POST)) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+
+        LoginRequest loginRequest = objectMapper.readValue(StreamUtils.copyToString(request.getInputStream(),
+                StandardCharsets.UTF_8), LoginRequest.class);
+
+        String username = loginRequest.getUserId();
+        String password = loginRequest.getUserPassword();
+
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            throw new AuthenticationServiceException("Missing required fields");
+        }
+
+        UsernamePasswordAuthenticationToken authRequest = UsernamePasswordAuthenticationToken.unauthenticated(username, password);
+        setDetails(request, authRequest);
+
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    protected void setDetails(HttpServletRequest request, UsernamePasswordAuthenticationToken authRequest) {
+        authRequest.setDetails(this.authenticationDetailsSource.buildDetails(request));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        authenticationSuccessHandler.onAuthenticationSuccess(request, response, authResult);
+    }
+}
