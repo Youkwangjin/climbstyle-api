21a73f2a77bd0c62e1c5e7e60c6788e0dc0f9605
Youkwangjin <kwangjin5468@naver.com>
2026-03-06 17:13:47 +0900

    Feat: 공지사항 엔터티 클래스 추가

---
+++ src/main/java/com/kwang/climbstyle/domain/notice/entity/NoticeEntity.java
@@ -0,0 +1,37 @@
+package com.kwang.climbstyle.domain.notice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoticeEntity {
+
+    private Integer noticeNo;
+
+    private Integer adminNo;
+
+    private String noticeTitle;
+
+    private String noticeContent;
+
+    private String noticeContentText;
+
+    private Integer noticeHit;
+
+    private Integer noticeFileCnt;
+
+    private String noticePinYn;
+
+    private String noticeVisibleYn;
+
+    private LocalDateTime noticeCreated;
+
+    private LocalDateTime noticeUpdated;
+}
