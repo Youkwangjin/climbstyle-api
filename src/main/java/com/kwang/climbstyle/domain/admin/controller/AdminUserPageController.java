9316e3803ec2e396aa6380eaad251175f3fbb417
Youkwangjin <kwangjin5468@naver.com>
2026-04-27 14:41:58 +0900

    Feat: 관리자용 유저 상세조회 API 구현

--- src/main/java/com/kwang/climbstyle/domain/admin/controller/AdminUserPageController.java
+++ src/main/java/com/kwang/climbstyle/domain/admin/controller/AdminUserPageController.java
@@ -2,11 +2,13 @@ package com.kwang.climbstyle.domain.admin.controller;
 
 import com.kwang.climbstyle.domain.admin.dto.request.AdminUserListRequest;
 import com.kwang.climbstyle.domain.admin.dto.response.AdminUserListResponse;
+import com.kwang.climbstyle.domain.user.dto.response.UserProfileResponse;
 import com.kwang.climbstyle.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -16,7 +18,7 @@ public class AdminUserPageController {
 
     private final UserService userService;
 
-    @GetMapping("/admin/users")
+    @GetMapping(value = "/admin/users")
     public String adminUserList(AdminUserListRequest request, Model model) {
         List<AdminUserListResponse> responses = userService.getAdminUserList(request);
 
@@ -24,5 +26,15 @@ public class AdminUserPageController {
         model.addAttribute("request", request);
 
         return "admin/user/list";
+    }
+
+    @GetMapping(value = "/admin/users/{userNo}")
+    public String adminUserDetail(@PathVariable("userNo") Integer userNo, AdminUserListRequest request, Model model) {
+        UserProfileResponse response = userService.selectUserByNo(userNo);
+
+        model.addAttribute("user", response);
+        model.addAttribute("request", request);
+
+        return "admin/user/detail";
     }
 }
