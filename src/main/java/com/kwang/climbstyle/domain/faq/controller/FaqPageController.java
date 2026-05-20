ac8c36530958d197d798a1292f4970de3eb18b11
Youkwangjin <kwangjin5468@naver.com>
2026-05-20 17:00:39 +0900

    Docs: FAQ 도메인 컨트롤러 및 서비스 주석 추가

--- src/main/java/com/kwang/climbstyle/domain/faq/controller/FaqPageController.java
+++ src/main/java/com/kwang/climbstyle/domain/faq/controller/FaqPageController.java
@@ -10,12 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+/**
+ * FAQ 페이지 컨트롤러
+ *
+ * @author : Youkwangjin
+ * @since : 2026-05-20
+ * @version : 1.0
+ */
 @Controller
 @RequiredArgsConstructor
 public class FaqPageController {
 
     private final FaqService faqService;
 
+    /**
+     * FAQ 목록 화면
+     * [ClimbStyle] > [FAQ]
+     */
     @GetMapping(value = "/faqs")
     public String faqList(CommonListRequest request, Model model) {
         List<FaqListResponse> responses = faqService.getFaqList(request);
