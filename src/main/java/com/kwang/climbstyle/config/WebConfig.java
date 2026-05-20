0db0fdc69ae3cb07bf9e50a95afd307d89bac90f
Youkwangjin <kwangjin5468@naver.com>
2026-05-20 17:25:15 +0900

    Docs: config 패키지 전체 Javadoc 주석 추가

--- src/main/java/com/kwang/climbstyle/config/WebConfig.java
+++ src/main/java/com/kwang/climbstyle/config/WebConfig.java
@@ -7,6 +7,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 정적 파일 리소스 핸들러 설정 (local 프로파일)
+ *
+ * @author : Youkwangjin
+ * @since : 2026-05-20
+ * @version : 1.0
+ */
 @Configuration
 @Profile("local")
 public class WebConfig implements WebMvcConfigurer {
