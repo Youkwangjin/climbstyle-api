a8b6ac0dd0125db5c3dd0005a624ddb72f7b09d2
Youkwangjin <kwangjin5468@naver.com>
2026-05-21 16:40:44 +0900

    Feat: 메인 페이지에 최신 피드·참여 가이드 섹션 추가, 다크 푸터 및 교차 배경 적용

--- src/main/java/com/kwang/climbstyle/domain/index/controller/IndexController.java
+++ src/main/java/com/kwang/climbstyle/domain/index/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.kwang.climbstyle.domain.index.controller;
 
 import com.kwang.climbstyle.domain.banner.dto.response.BannerDetailResponse;
 import com.kwang.climbstyle.domain.banner.service.BannerService;
+import com.kwang.climbstyle.domain.feed.dto.response.FeedListResponse;
+import com.kwang.climbstyle.domain.feed.service.FeedService;
 import com.kwang.climbstyle.domain.ranking.dto.response.RankingListResponse;
 import com.kwang.climbstyle.domain.ranking.service.RankingService;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,8 @@ public class IndexController {
 
     private final BannerService bannerService;
 
+    private final FeedService feedService;
+
     /**
      * 홈 화면
      * [ClimbStyle] > 홈
@@ -34,9 +38,12 @@ public class IndexController {
     public String index(Model model) {
         List<RankingListResponse> rankingListResponse = rankingService.getIndexRealtimeRankingList();
         List<BannerDetailResponse> bannerList = bannerService.getVisibleBanners();
+        List<FeedListResponse> latestFeeds = feedService.getLatestFeeds(6);
 
         model.addAttribute("realtimeTop3", rankingListResponse);
         model.addAttribute("bannerList", bannerList);
+        model.addAttribute("latestFeeds", latestFeeds);
+
         return "index";
     }
 
